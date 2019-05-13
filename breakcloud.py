d84430c6b24345fab055a27c4de077942dcb0234
Alex <alex.kartun@gmail.com>
2019-05-13 17:38:51 +0300

    Final Commit!

--- breakcloud.py
+++ breakcloud.py
@@ -1,6 +1,5 @@
-# constants
-# we know that, cipher_byte = F(k, CTR) xor plain_byte, so purpose of zero_byte is to reveal F(k, CTR) value
-ZERO_BYTE = b'\x00'
+# we know ctr mode encryption and decryption are the same operations, so encrypt cypher text will give you
+# the original decrypted message
 OUTPUT_PATH = 'plain.txt'
 
 
@@ -11,16 +10,15 @@ def breakcloud(cloud):
     :return: None. writing result plain_text to file (OUTPUT_PATH)
     """
     position = 0
-    plain_text = []
-
+    cipher_length = cloud.Length()
+    plain_text = bytearray(cipher_length)
     # iterate over all the cipher_text till position (counter) will overflow the cipher_text length
-    while position < cloud.Length():
-        cipher_byte = cloud.Write(position, ZERO_BYTE)  # get cipher byte and write ZERO_BYTE
-        decrypted_zero_byte = cloud.Read(position)  # read decrypted ZERO_BYTE which value is F(k, CTR)
-
-        # plain_byte = F(k, CTR) xor cipher_byte
-        plain_text.append(chr(ord(decrypted_zero_byte) ^ ord(cipher_byte)))
+    while position < cipher_length:
+        cipher_byte = cloud.Read(position)      # get cipher byte
+        _ = cloud.Write(position, cipher_byte)  # write cipher_byte
+        plain_byte = cloud.Read(position)       # read the encrypted cipher_byte to get plain_byte
+        plain_text[position] = plain_byte
         position += 1
 
-    with open(OUTPUT_PATH, mode='wb') as f:  # write to file all the plain_text content
-        f.write(''.join(plain_text))
+    with open(OUTPUT_PATH, mode='wb') as f:     # write to file all the plain_text content
+        f.write(plain_text)
--- cloud.py
+++ cloud.py
@@ -16,23 +16,25 @@ class Cloud:
         self.__nonce = nonce
         with open(filename, mode='rb') as f:
             plain_text = f.read()
-        self.__cipher_text = self.__encrypt(plain_text)
+        self.__cipher_text = bytearray(self.__encrypt(plain_text))
 
-    def __create_cipher(self):
+    def __create_cipher(self, init_values):
         """
         creating aes cipher via ctr mode and 256 bit key
+        :param init_values: initial value of counter
         :return: aes cipher
         """
-        counter = Counter.new(COUNTER_BIT_SIZE, self.__nonce)
+        counter = Counter.new(COUNTER_BIT_SIZE, self.__nonce, initial_value=init_values)
         return AES.new(self.__key, AES.MODE_CTR, counter=counter)
 
-    def __encrypt(self, data):
+    def __encrypt(self, data, init_values=1):
         """
         encrypt via ctr mode
         :param data: data to be encrypted
+        :param init_values: initial value of counter (default is 1)
         :return: encrypted data
         """
-        cipher = self.__create_cipher()
+        cipher = self.__create_cipher(init_values)
         return cipher.encrypt(data)
 
     def Length(self):
@@ -48,19 +50,23 @@ class Cloud:
         :param position: encrypted byte's location
         :return: encrypted byte
         """
-        return chr(ord(self.__cipher_text[position]))
+        return chr(self.__cipher_text[position])
 
     def Write(self, position=0, new_byte='\x33'):
         """
-        write the plain byte to cipher_text at specific position user provided
-        :param position: encrypted byte's location user provided
-        :param new_byte: new plain byte we want to decrypt and write to cipher_text
+        write new byte to cipher_text at specific position user provided
+        :param position: position of new byte
+        :param new_byte: new byte we want to decrypt and write to cipher_text at specific position
         :return: old encrypted byte at position
         """
-
         old_byte = self.Read(position)
-        plain_text = self.__encrypt(self.__cipher_text)  # decrypt back to plain_text
-        plain_text = plain_text[:position] + new_byte + plain_text[position + 1:]  # change byte in position
-        self.__cipher_text = self.__encrypt(plain_text)  # encrypt back to cypher_text
+        block_index = (position / AES.block_size) + 1
+        block_offset = position % AES.block_size
+        empty_aes_block = bytearray(AES.block_size)     # empty aes block
+        empty_aes_block[block_offset] = new_byte        # set new byte in empty block at offset
+        # encrypt the block
+        encrypted_aes_block = bytearray(self.__encrypt(str(empty_aes_block), init_values=block_index))
+        # set encrypted byte from encrypted block to cipher_text byte array at position provided
+        self.__cipher_text[position] = encrypted_aes_block[block_offset]
 
         return old_byte
