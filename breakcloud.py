225c9f7fa6220e9b93d82b441631f329f04f1a8b
Alex <alex.kartun@gmail.com>
2019-05-14 21:36:05 +0300

    Final Commit!

--- breakcloud.py
+++ breakcloud.py
@@ -1,6 +1,5 @@
-# we know ctr mode encryption and decryption are the same operations, so encrypt cypher text will give you
-# the original decrypted message
 OUTPUT_PATH = 'plain.txt'
+ZERO_BYTE = b'\x00'
 
 
 def breakcloud(cloud):
@@ -14,10 +13,9 @@ def breakcloud(cloud):
     plain_text = bytearray(cipher_length)
     # iterate over all the cipher_text till position (counter) will overflow the cipher_text length
     while position < cipher_length:
-        cipher_byte = cloud.Read(position)      # get cipher byte
-        _ = cloud.Write(position, cipher_byte)  # write cipher_byte
-        plain_byte = cloud.Read(position)       # read the encrypted cipher_byte to get plain_byte
-        plain_text[position] = plain_byte
+        c = cloud.Write(position, ZERO_BYTE)            # encrypt zero byte
+        m_tag = cloud.Read(position)                    # m_tag = F(k, CTR) xor 0 = F(k, CTR)
+        plain_text[position] = ord(m_tag) ^ ord(c)      # m_b = m_tag xor c
         position += 1
 
     with open(OUTPUT_PATH, mode='wb') as f:     # write to file all the plain_text content
